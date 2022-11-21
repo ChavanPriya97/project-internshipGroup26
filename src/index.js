6114d34233f7a6898c1cf53aa96d44b7ac6fbbca
ChavanPriya97 <priya971110@gmail.com>
2022-11-21 12:36:36 +0530

    done main

---
+++ src/index.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const route = require("./routes/route");
+const app = express();
+
+app.use(bodyParser.json());
+
+mongoose
+  .connect(
+    "mongodb+srv://project2_intershipGroup:project2@cluster0.xvahxxz.mongodb.net/Group25Database",
+    { useNewUrlParser: true }
+  )
+  .then(() => console.log("Project2 mongoDB is connected"))
+  .catch((err) => console.log({ error: err.message }));
+
+app.use("/", route);
+
+app.listen(process.env.PORT || 3000, function () {
+  console.log("express run on server " + (process.env.PORT || 3000));
+});
